2988895c22fc06cc012c86290f76267edb600be2
Vickyadri29 <vickyadri290501@gmail.com>
2022-05-06 23:44:07 +0800

    setting routes in server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// Call Routes
+const routes = require('./routes');
+routes(app);
+
 app.listen(3000, () => {
     console.log('Server is running...')
 })
